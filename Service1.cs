22ad2ae655a16c3f3c3ef91bd4b6937aaf7ef5fd
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-11-18 09:13:08 -0300

    Adicionar arquivos de projeto.

---
+++ Service1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceEmail
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer1.Enabled = true;
+            timer1_Tick(null, null);
+        }
+
+        protected override void OnStop()
+        {
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            AvisoRessuprimentos.Model.AvisoEmail oAviso = new AvisoRessuprimentos.Model.AvisoEmail();
+            oAviso.EnviarAviso();
+            timer1.Enabled = true;
+        }
+    }
+}
